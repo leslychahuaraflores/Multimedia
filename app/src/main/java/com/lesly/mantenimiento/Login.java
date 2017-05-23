194603bc79860808dffc4413510cd2f6c0a91002
g10vlad <g10vladimir@gmail.com>
2017-05-23 08:02:17 -0500

    multimedia

---
+++ app/src/main/java/com/lesly/mantenimiento/Login.java
@@ -0,0 +1,82 @@
+package com.lesly.mantenimiento;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        Button btnIngresar = (Button) findViewById(R.id.btnEntrar);
+
+
+        btnIngresar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void  onClick(View view){
+                String usuario = ((EditText)findViewById(R.id.usuario)).getText().toString();
+                String password = ((EditText)findViewById(R.id.password)).getText().toString();
+                if (usuario.equals("Admin")&& password.equals("123456")){
+                    Intent newform =new Intent(Login.this, com.lesly.mantenimiento.Register.class);
+                    startActivity(newform);
+
+                }else{
+                    Toast.makeText(getApplicationContext(),"<<<< Clave Incorrecta >>>>",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/lesly/mantenimiento/Registro.java
@@ -0,0 +1,8 @@
+package com.lesly.mantenimiento;
+
+/**
+ * Created by Alumnos on 16/05/2017.
+ */
+
+public class Registro {
+}
